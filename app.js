ecc939531082a1558e6a705161d6796dd92388cc
rohit <rohit@one.com>
2023-06-08 19:15:17 +0400

    Episode 1

---
+++ app.js
@@ -0,0 +1,14 @@
+/**creating sibling elements using react */
+
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "test"),
+    React.createElement("h2", {}, "test"),
+  ])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
